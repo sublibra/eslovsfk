c1610806641dd1d5ce3b7a7c7e5d4d3d18a29aa7
Jonas Hörström <jonas.horstrom@gmail.com>
2023-04-05 22:26:19 +0200

    Adding sftp upload, more config and headless browser

--- get-calendar.py
+++ get-calendar.py
@@ -1,36 +1,39 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from datetime import datetime
-import shutil
 from decouple import config
 import pysftp
-
+import os
 
 ## Login idrottonline
-browser = webdriver.Firefox()
-browser.get('https://login.idrottonline.se')
-user_name = browser.find_element(By.ID, 'userName') 
-user_name.send_keys(config('USERNAME'))
-password = browser.find_element(By.NAME, 'password') 
-password.send_keys(config('PASSWORD'))
-submit = browser.find_element(By.XPATH, "//form/button") #loginForm > button
+firefox_options = Options()
+firefox_options.add_argument("--headless")
+driver = webdriver.Chrome(service=Service('/usr/local/bin/geckodriver'), options=firefox_options)
+driver.get('https://login.idrottonline.se')
+user_name = driver.find_element(By.ID, 'userName') 
+user_name.send_keys(config('IDROTT-USER'))
+password = driver.find_element(By.NAME, 'password') 
+password.send_keys(config('IDROTT-PASS'))
+submit = driver.find_element(By.XPATH, "//form/button") #loginForm > button
 submit.click()
 time.sleep(7) ## Many many redirects
 
 ## Export calendar
 calendar_string = "https://activity.idrottonline.se/activities/exportactivitiestoical?calendarId=7140&startTime={0}-01-01+00%3A00%3A00&endTime={0}-12-31+00%3A00%3A00&freeText=&activityTypes="
-browser.get(calendar_string.format(datetime.today().strftime('%Y')))
+driver.get(calendar_string.format(datetime.today().strftime('%Y')))
 time.sleep(4) ## Wait for file
-browser.close()
+driver.quit()
 
 ## Copy file to home page
-#with pysftp.Connection('hostname', username='me', password='secret') as sftp:
-
-## Clean-up
-try:
-  shutil.rmtree('~/Downloads/Kalender.ics')
-except EnvironmentError:
-    print("Couldn't delete file")
-
+cal_file = os.path.expanduser( '~' ) + '/Downloads/Kalender.ics'
+if os.path.exists(cal_file):
+  with pysftp.Connection(config('SFTP-HOST'), username=config('SFTP-USER'), password=config('SFTP-PASS')) as sftp:
+   with sftp.cd('wp-content/uploads'): 
+      sftp.put(cal_file)  
+  os.remove(cal_file)
+else:
+  print('Failed to retrieve or find the file: {0}'.format(cal_file))
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip install selenium
+pip install pysftp
+brew install geckodriver # macos
